be5b4ecaa05829e46f49b7155d2d660daa2a8fc0
Mubashira Qari <81127634+uzmabb182@users.noreply.github.com>
2022-01-23 23:58:36 -0500

    Database queries added

--- MQari_SQL_Bridge_Final_Project/QuickDBD-keycard_schema.sql
+++ MQari_SQL_Bridge_Final_Project/QuickDBD-keycard_schema.sql
@@ -9,6 +9,7 @@
 DROP DATABASE keycard_db;
 CREATE DATABASE keycard_db;
 
+DROP TABLE users;
 CREATE TABLE users (
     user_id int  NOT NULL AUTO_INCREMENT ,
     user_name varchar(30)  NOT NULL ,
@@ -33,6 +34,7 @@ CREATE TABLE rooms (
     )
 );
 
+DROP TABLE group_assignment;
 CREATE TABLE group_assignment (
     id int  NOT NULL AUTO_INCREMENT,
     group_id int  NOT NULL ,
@@ -43,6 +45,7 @@ CREATE TABLE group_assignment (
     )
 );
 
+DROP TABLE room_assignment;
 CREATE TABLE room_assignment (
     id int  NOT NULL AUTO_INCREMENT,
     room_id int  NOT NULL ,
@@ -65,3 +68,83 @@ REFERENCES rooms (room_id);
 ALTER TABLE room_assignment ADD CONSTRAINT fk_room_assignment_group_id FOREIGN KEY(group_id)
 REFERENCES key_groups (group_id);
 
+SELECT * 
+FROM users;
+
+INSERT INTO users(user_name)
+VALUES ('Modesto'),
+('Ayine'),
+('Cheong Woo'),
+('Heidy'),
+('Saulet');
+
+INSERT INTO users(user_name)
+VALUES ('Christopher');
+
+SELECT * 
+FROM key_groups;
+
+INSERT INTO key_groups(group_name)
+VALUES ('I.T.'),
+('Sales'),
+('Administration'),
+('Operation');
+
+SELECT * 
+FROM rooms;
+
+INSERT INTO rooms(room_name)
+VALUES ('Room 101'),
+('Room 102'),
+('Auditorium A'),
+('Auditorium B');
+
+SELECT * 
+FROM room_assignment;
+
+
+INSERT INTO room_assignment(room_id, group_id, assigned_room)
+VALUES (1, 1, 'Room 101'),
+(2, 1, 'Room 102'),
+(2, 2, 'Room 102'),
+(3, 2, 'Auditorium A'),
+(3, 4, 'Auditorium A'),
+(4, 4, 'Auditorium B');
+
+SELECT * 
+FROM group_assignment;
+
+
+INSERT INTO group_assignment(group_id, user_id, assigned_group)
+VALUES (1, 1, 'I.T.'),
+(1, 2, 'I.T.'),
+(2, 3, 'Sales'),
+(2, 4, 'Sales'),
+(3, 5, 'Administration');
+
+
+
+-- write SELECT statements that provide the following information:
+-- All groups, and the users in each group. A group should appear even if there are no users assigned to the group.
+ SELECT k.group_name, u.user_name
+ FROM key_groups k, users u, group_assignment ga
+ WHERE k.group_id = ga.group_id
+ AND u.user_id = ga.user_id;
+ 
+ -- All rooms, and the groups assigned to each room. The rooms should appear even if no groups have been
+-- assigned to them.
+ 
+SELECT g.group_name, r.room_name
+FROM rooms r, key_groups g, room_assignment ra
+WHERE r.room_id = ra.room_id
+AND g.group_id = ra.group_id;
+ 
+ -- A list of users, the groups that they belong to, and the rooms to which they are assigned. This should be sorted
+-- alphabetically by user, then by group, then by room.
+ SELECT u.user_name, g.group_name, r.room_name
+ FROM users u, key_groups g, rooms r, room_assignment ra, group_assignment ga
+ WHERE r.room_id = ra.room_id
+ AND g.group_id = ga.group_id
+ AND u.user_id = ga.user_id
+ ORDER BY u.user_name, g.group_name, r.room_name;
+ 
