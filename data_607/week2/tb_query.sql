5846392ef5afa064750a448ed90e22d3e14bdc65
Mubashira Qari <81127634+uzmabb182@users.noreply.github.com>
2022-02-11 07:50:56 -0500

    tb_data is uploaded in PostgreSQL

---
+++ data_607/week2/tb_query.sql
@@ -0,0 +1,32 @@
+/*
+  tb.sql
+*/
+
+DROP TABLE IF EXISTS tb;
+
+CREATE TABLE tb 
+(
+  country varchar(100) NOT NULL,
+  year int NOT NULL,
+  sex varchar(6) NOT NULL,
+  child int NULL,
+  adult int NULL,
+  elderly int NULL
+);
+
+SELECT * FROM tb;
+
+LOAD DATA INFILE 'c:/data/tb.csv' 
+INTO TABLE tb
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+(country, year, sex, @child, @adult, @elderly)
+SET
+child = nullif(@child,-1),
+adult = nullif(@adult,-1),
+elderly = nullif(@elderly,-1)
+;
+
+SELECT * FROM tb WHERE elderly IS NULL;
+SELECT COUNT(*) FROM tb;
